0662078cc0801fe57d32cbf76b67cb17728689ff
Admin <mercerbe@gmail.com>
2018-07-29 16:59:23 -0400

    added server.js setup

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -3,20 +3,21 @@ import React, { Component } from 'react'
 
 class Home extends Component {
   constuctor() {
+    //pull super
     super()
   }
 
   //render home
   render() {
     const imageStyle = {
-      width=400
+      width: 400
     }
     return (
       <div>
         <p>
           Welcome Home!
         </p>
-        <img style='{imageStyle} src='/>
+        <img style={imageStyle} src='' />
       </div>
     )
   }
